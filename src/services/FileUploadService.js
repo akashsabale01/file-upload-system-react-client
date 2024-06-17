550c6799829a408638afc7dc5d5fcc7870fea368
akashsabale01 <akashsabale100@gmail.com>
2024-06-18 00:46:50 +0530

    updated service

--- src/services/FileUploadService.js
+++ src/services/FileUploadService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'https://localhost:7254/api/Files';
-const API_URL = import.meta.env.VITE_API_BASIC_URL_DEPLOYED;
+const API_URL = import.meta.env.VITE_API_BASIC_URL_DEPLOYED + '/api/Files';
 
 const getAllFiles = () => axios.get(API_URL);
 
